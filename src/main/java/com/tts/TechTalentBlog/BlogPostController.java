472d9a2cc46a9ab31f6884dcca4e94b7109b2185
brightcodedev <info@brightcode.dev>
2020-04-08 11:56:24 -0400

    added update method

--- src/main/java/com/tts/TechTalentBlog/BlogPostController.java
+++ src/main/java/com/tts/TechTalentBlog/BlogPostController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class BlogPostController {
@@ -18,7 +20,18 @@ public class BlogPostController {
 	
 	@GetMapping("/")
 	public String index(BlogPost blogPost, Model model) {
-		model.addAttribute("posts", posts);
+		
+		// What I used to fix issue: This is returning from our h2 database
+		
+		List<BlogPost> postsToDisplay = blogPostRepository.findAll(); 
+		model.addAttribute("posts", postsToDisplay);
+		
+		//
+		
+		// What we had: This is returning from a static variable
+		// model.addAttribute("posts", posts);
+		//
+		
 		return "blogpost/index";
 	}
 	
@@ -33,6 +46,27 @@ public class BlogPostController {
 		posts.add(savedPost);
 		model.addAttribute("post", savedPost);
 		return "blogpost/result";
+	}
+	
+	@GetMapping("/posts/update/{id}")
+	public String updateForm(BlogPost blogPost, @PathVariable Long id, Model model) {
+		model.addAttribute("postIdToChange", id);
+		return "blogpost/update";
+	}
+	
+	@PostMapping("/posts/update/{id}")
+	public String updatePost(BlogPost blogPost, Model model, @PathVariable Long id) {
+
+		BlogPost blogPostById = blogPostRepository.findBlogPostById(id);
+		
+		blogPostById.setTitle(blogPost.getTitle());
+		blogPostById.setAuthor(blogPost.getAuthor());
+		blogPostById.setBlogEntry(blogPost.getBlogEntry());
+		
+		blogPostRepository.save(blogPostById);
+		// I also made an adjustment. Instead of loading the file I wanted to redirect instread
+		return "redirect:/";
+		//
 	}
 	
 }
--- src/main/java/com/tts/TechTalentBlog/BlogPostRepository.java
+++ src/main/java/com/tts/TechTalentBlog/BlogPostRepository.java
@@ -1,7 +1,7 @@
 package com.tts.TechTalentBlog;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
-	
+public interface BlogPostRepository extends JpaRepository<BlogPost, Long>{
+	BlogPost findBlogPostById(Long id);
 }
