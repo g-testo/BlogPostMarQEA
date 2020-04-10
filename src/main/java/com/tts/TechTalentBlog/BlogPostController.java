23dc32e70a52d8dc255af07a7f67d3f4e22b3519
brightcodedev <info@brightcode.dev>
2020-04-09 21:45:14 -0400

    added fragments

--- src/main/java/com/tts/TechTalentBlog/BlogPostController.java
+++ src/main/java/com/tts/TechTalentBlog/BlogPostController.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BlogPostController {
@@ -17,29 +20,27 @@ public class BlogPostController {
 	
 	@Autowired
 	private BlogPostRepository blogPostRepository;
-	
+	@CrossOrigin()
 	@GetMapping("/")
-	public String index(BlogPost blogPost, Model model) {
+	public String index(Model model) {
 		
-		// What I used to fix issue: This is returning from our h2 database
+//		 What I used to fix issue: This is returning from our h2 database
 		
 		List<BlogPost> postsToDisplay = blogPostRepository.findAll(); 
 		model.addAttribute("posts", postsToDisplay);
 		
-		//
+//		//	What we had: This is returning from a static variable
+//		model.addAttribute("posts", posts);
 		
-		// What we had: This is returning from a static variable
-		// model.addAttribute("posts", posts);
-		//
 		
 		return "blogpost/index";
 	}
-	
+	@CrossOrigin()
 	@GetMapping("/posts/new")
 	public String newPost (BlogPost blogPost) {
 		return "blogpost/new";
 	}
-	
+	@CrossOrigin()
 	@PostMapping("/posts")
 	public String addPost(BlogPost blogPost, Model model) {
 		BlogPost savedPost = blogPostRepository.save(blogPost);
@@ -47,13 +48,13 @@ public class BlogPostController {
 		model.addAttribute("post", savedPost);
 		return "blogpost/result";
 	}
-	
+	@CrossOrigin()
 	@GetMapping("/posts/update/{id}")
 	public String updateForm(BlogPost blogPost, @PathVariable Long id, Model model) {
 		model.addAttribute("postIdToChange", id);
 		return "blogpost/update";
 	}
-	
+	@CrossOrigin()
 	@PostMapping("/posts/update/{id}")
 	public String updatePost(BlogPost blogPost, Model model, @PathVariable Long id) {
 
@@ -64,9 +65,18 @@ public class BlogPostController {
 		blogPostById.setBlogEntry(blogPost.getBlogEntry());
 		
 		blogPostRepository.save(blogPostById);
-		// I also made an adjustment. Instead of loading the file I wanted to redirect instread
+		
+		
+		// I also made an adjustment. Instead of loading the file I wanted to redirect instead
 		return "redirect:/";
 		//
 	}
+	@CrossOrigin()
+	
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.DELETE)
+    public String deletePostWithId(@PathVariable Long id, BlogPost blogPost) {
+        blogPostRepository.deleteById(id);
+        return "redirect:/posts/new";
+    }
 	
 }
--- src/main/java/com/tts/TechTalentBlog/TechTalentBlogApplication.java
+++ src/main/java/com/tts/TechTalentBlog/TechTalentBlogApplication.java
@@ -2,12 +2,19 @@ package com.tts.TechTalentBlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class TechTalentBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechTalentBlogApplication.class, args);
+	}
+	
+	@Bean
+	HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+	    return new HiddenHttpMethodFilter();
 	}
 
 }
